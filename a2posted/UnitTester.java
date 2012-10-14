1c4bde0dd7cab2ea151e06213dc953feb14920b1
Dominic Charley-Roy <dominiccharleyroy@gmail.com>
2012-10-14 09:31:47 -0400

    Added more test cases.

--- a2posted/UnitTester.java
+++ a2posted/UnitTester.java
@@ -6,7 +6,7 @@ public class UnitTester {
 
 	public static void main(String... args) {
 		HashMap<String, Boolean> testCases = new HashMap<String, Boolean>();
-        
+
 		testCases.put("foo=23", true);
 		testCases.put("foo= 23", false);
 		testCases.put("foo =23", false);
@@ -22,11 +22,10 @@ public class UnitTester {
 		testCases.put("then=23", true);
 		testCases.put("else=23", true);
 		testCases.put("end=23", true);
-		testCases.put("if true then if false then a=2 else b=12 end else a=10 end", true); 
+		testCases.put("if true then if false then a=2 else b=12 end else a=10 end", true);
 		testCases.put("if true then if false then a=2 else b=12 end else if false then a=2 else b=12 end end", true);
 		testCases.put("if true then if false then if true then if false then a=2 else b=12 end else if false then a=2 else b=12 end end else b=12 end else if false then a=2 else if true then if false then a=2 else b=12 end else if false then a=2 else b=12 end end end end", true);
-		
-        testCases.put("if true then a=2 else b=31 end", true);
+		testCases.put("if true then a=2 else b=31 end", true);
 		testCases.put("if false then if true then c=5 else d=5 end else b=31 end", true);
 		testCases.put("if true then a=2 else b=31", false);
 		testCases.put("if a=2 then a=2 else b=31 end", false);
@@ -40,31 +39,41 @@ public class UnitTester {
 		testCases.put("123", false);
 		testCases.put("if if", false);
 		testCases.put("if true then a=1 else b=1 end end", false);
-        testCases.put("end", false); //just end
-        testCases.put("if a=2 then a=2 else a=2 end", false); //non bool
-        testCases.put("if true then a=3 else a=2 end end", false); //double end
-        testCases.put("if else end", false); // just if else end no statement
-        testCases.put("a=3 a=2", false); // double assignment
-        testCases.put("if true then a=3 a=2 else a=3 end", false); 
-        // double assignment inside conditionnal statement
-        testCases.put("if true then a=3 else a=3 a=2 end", false);
-        testCases.put("if false then a=3 else a=2 end a=3", false); 
-        //assignment after "end"
-		
+		testCases.put("end", false); // just end
+		testCases.put("if a=2 then a=2 else a=2 end", false); // non bool
+		testCases.put("if true then a=3 else a=2 end end", false); // double end
+		testCases.put("if else end", false); // just if else end no statement
+		testCases.put("a=3 a=2", false); // double assignment
+		testCases.put("if true then a=3 a=2 else a=3 end", false);
+		// double assignment inside conditionnal statement
+		testCases.put("if true then a=3 else a=3 a=2 end", false);
+		testCases.put("if false then a=3 else a=2 end a=3", false);
+		// assignment after "end"
+		testCases.put("if if then a=2 else a=3 end", false);
+		testCases.put("if else then a=2 else a=3 end", false);
+		testCases.put("if end then a=2 else a=3 end", false);
+		testCases.put("if true then then a=2 else a=3 end", false);
+		testCases.put("if true then a=2 else else a=3 end", false);
+		testCases.put("if end", false);
+		testCases.put("if true then a=5 else b=2 end if true then a=5 else b=7 end if true then a=5 else b=7 end if true then a=5 else b=7 end", false);
+
 		StringSplitter splitter;
-		
+
 		int fails = 0;
-		
+
 		for (String testCase : testCases.keySet()) {
 			splitter = new StringSplitter(testCase);
 			if (LanguageParser.parse(splitter) != testCases.get(testCase)) {
 				fails++;
-				System.out.println("Test case \"" + testCase +"\" failed!\nExpected result: " + 
-						testCases.get(testCase) + "\nObtained result: " + !testCases.get(testCase) + "\n");
+				System.out.println("Test case \"" + testCase
+						+ "\" failed!\nExpected result: "
+						+ testCases.get(testCase) + "\nObtained result: "
+						+ !testCases.get(testCase) + "\n");
 			}
 		}
-		
-		System.out.println((testCases.size() - fails) + "/" + testCases.size() + " test cases passed.");
+
+		System.out.println((testCases.size() - fails) + "/"
+				+ testCases.size() + " test cases passed.");
 	}
-	
+
 }
